041cd852d38949161f275a41e81cd0e1dc5949cd
efciapel <owadodestruktor@gmail.com>
2016-11-16 21:53:52 +0100

    Zarys biblioteki

--- __init__.py
+++ __init__.py
@@ -1,1 +1,60 @@
-import numpy
+def read_fasta(file):
+    """Odczytuje plik FASTA.
+    Args:
+        file (file): plik FASTA.
+    """
+
+
+def read_dna(dna):
+    """Odczytuje sekwencje DNA.
+    Args:
+        dna (str): sekwencja nukleotydowa.
+    """
+
+
+def length(dna):
+    """Zwraca długość podanej sekwencji.
+    Args:
+        dna (str): sekwencja nukleotydowa.
+    """
+
+
+def nucleotide_occurence(*args):
+    """Podaje procentowe występowanie danego nukleotydu w sekwencji lub grupy
+    nukleotydów."""
+
+
+def complement(dna):
+    """Zwraca nić komplementarną sekwencji.
+    Args:
+        dna (str): sekwencja nukleotydowa.
+    """
+
+
+def reverse_complement(dna):
+    """Zwraca odwróconą nić komplementarną sekwencji.
+    Args:
+        dna (str): sekwencja nukleotydowa.
+    """
+
+
+def molecular_mass(dna):
+    """Zwraca nić masę cząsteczkową łańcucha.
+    Args:
+        dna (str): sekwencja nukleotydowa.
+    """
+
+
+def melting_point(dna):
+    """Zwraca temperaturę topnienia sekwencji.
+    Args:
+        dna (str): sekwencja nukleotydowa.
+    """
+
+
+def alignment(dna_1, dna_2):
+    """Zwraca nić score przyrównania dwóch sekwencji.
+    Wykorzystuje algorytm Needlemana-Wunscha.
+    Args:
+        dna (str): sekwencja nukleotydowa.
+    """
