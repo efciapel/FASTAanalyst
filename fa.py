fed759fb23fb990fdc1b12c8564c2cdddd0d7c7a
MKepska <martynakepska@wp.pl>
2017-01-15 23:21:56 +0100

    Dodanie funkcji 'complement', 'reverse_complement' i 'melting_point'.

--- fa.py
+++ fa.py
@@ -40,11 +40,20 @@ def read_dna(dna):
     return dna.lower()
 
 
-def length(dna):
+def sequence_type(seq):
+    seq_type = 'DNA'
+
+    if seq.find('t') != -1:
+        seq_type = 'RNA'
+
+    return seq_type
+
+
+def length(seq):
     # Anna Osina
     """Zwraca długość podanej sekwencji.
     Args:
-        dna (str): sekwencja nukleotydowa.
+        seq (str): sekwencja nukleotydowa.
     """
 
 
@@ -54,39 +63,57 @@ def nucleotide_occurence(*args):
     nukleotydów."""
 
 
-def complement(dna):
-    # Martyna Kępska
+def complement(seq):
     """Zwraca nić komplementarną sekwencji.
     Args:
-        dna (str): sekwencja nukleotydowa.
+        seq (str): sekwencja nukleotydowa.
     """
+    nucleotides = {'c': 'g', 'g': 'c'}
+    ret = ''
+    seq_type = sequence_type(seq)
+
+    if seq_type == 'RNA':
+        nucleotides.update({'a': 'u', 'u': 'a'})
+    else:
+        nucleotides.update({'a': 't', 't': 'a'})
+
+    for base in seq:
+        ret += nucleotides.get(base)
+
+    return ret
 
 
-def reverse_complement(dna):
-    # Martyna Kępska
+def reverse_complement(seq):
     """Zwraca odwróconą nić komplementarną sekwencji.
     Args:
-        dna (str): sekwencja nukleotydowa.
+        seq (str): sekwencja nukleotydowa.
     """
+    return complement(seq)[::-1]
 
 
-def molecular_mass(dna):
+def molecular_mass(seq):
     # Anna Osina
     """Zwraca nić masę cząsteczkową łańcucha.
     Args:
-        dna (str): sekwencja nukleotydowa.
+        seq (str): sekwencja nukleotydowa.
     """
 
 
-def melting_point(dna):
-    # Martyna Kępska
+def melting_point(seq):
     """Zwraca temperaturę topnienia sekwencji.
     Args:
-        dna (str): sekwencja nukleotydowa.
+        seq (str): sekwencja nukleotydowa.
     """
+    seq_type = sequence_type(seq)
+    count_a = seq.count('a')
+    count_c = seq.count('c')
+    count_g = seq.count('g')
+    count_ut = seq.count('t') if seq_type == 'DNA' else seq.count('u')
+
+    return 4 * (count_g + count_c) + 2 * (count_a + count_ut)
 
 
-def alignment(dna_1, dna_2):
+def alignment(seq_1, seq_2):
     # Ewa Król
     """Zwraca nić score przyrównania dwóch sekwencji.
     Wykorzystuje algorytm Needlemana-Wunscha.
